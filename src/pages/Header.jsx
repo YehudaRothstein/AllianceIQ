ce85e82e1fee6891fb55cc0cd6717dd80001c4bc
Team <yehudarothstein@gmail.com>
2024-12-30 21:24:24 +0200

    add: made navtext route to home

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,19 +1,24 @@
-// src/pages/Header.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Button, Typography } from '@mui/material';
 
 function Header() {
     return (
-        <AppBar position="sticky" color='inherit' sx={{ boxShadow: 'none' }}>
+        <AppBar position="sticky" color="inherit" sx={{ boxShadow: 'none' }}>
             <Toolbar>
-                <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                <Typography
+                    variant="h6"
+                    sx={{ flexGrow: 1 }}
+                    component={Link} // Make the Typography clickable
+                    to="/" // Link to the home page
+                    style={{ textDecoration: 'none', color: 'inherit' }} // Ensure no default styling is applied to the link
+                >
                     AllianceIQ
                 </Typography>
-                <Button color="inherit" component={Link} to="/">
-                    Home
+                <Button color="inherit" component={Link} to="/contact">
+                    Contact
                 </Button>
-                <Button color="inherit" component={Link} to="/stats"> {/* Contact link */}
+                <Button color="inherit" component={Link} to="/stats">
                     Statistics
                 </Button>
             </Toolbar>
